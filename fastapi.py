c6cd60af603a77c725d89cf802ce8936f8d5b2b7
Umar <79453927+umar052001@users.noreply.github.com>
2023-09-14 15:26:01 +0500

    Update fastapi.py

--- fastapi.py
+++ fastapi.py
@@ -1,86 +1,119 @@
-from fastapi import FastAPI, HTTPException
-from decouple import config
-import httpx
+from fastapi import FastAPI, Form, File, UploadFile
+from pydantic import BaseModel
+from typing import List, Optional
+import tempfile
+import os
+import torchaudio
+import torch
+from tortoise.api import TextToSpeech
+from tortoise.utils.audio import load_audio, load_voice
 
 app = FastAPI()
 
-# Load the Gradio API URL and key from the .env file
-gradio_url = config("GRADIO_API_URL")
-gradio_api_token = config("GRADIO_API_KEY")
+# Initialize the TextToSpeech model
+tts = TextToSpeech()
 
-async def make_gradio_request(input_1: str, input_2: str, input_3: str, fn_index: int):
-    """
-    Make a request to the Gradio API with provided input data and fn_index.
+# Constants
+VOICES = ["random", "train_atkins", "train_daws", "train_dotrice", "train_dreams", "train_empire", "train_grace", "train_kennard", "train_lescault", "train_mouse", "angie", "applejack", "daniel", "deniro", "emma", "freeman", "geralt", "halle", "jlaw", "lj", "mol", "myself", "pat", "pat2", "rainbow", "snakes", "tim_reynolds", "tom", "weaver", "william"]
+DEFAULT_VOICE = "random"
+PRESETS = ["ultra_fast", "fast", "standard", "high_quality"]
+DEFAULT_PRESET = "fast"
+DEFAULT_TEXT = "Hello, world!"
 
-    Args:
-        input_1 (str): The first input parameter.
-        input_2 (str): The second input parameter.
-        input_3 (str): The third input parameter.
-        fn_index (int): The fn_index parameter for the Gradio API.
+TORTOISE_SR_IN = 22050
+TORTOISE_SR_OUT = 24000
 
-    Returns:
-        str: Response from the Gradio API.
-    """
-    async with httpx.AsyncClient() as client:
-        headers = {
-            "Authorization": f"Bearer {gradio_api_token}"  # Replace with the appropriate authentication method
-        }
-        payload = {
-            "input_1": input_1,
-            "input_2": input_2,
-            "input_3": input_3,
-            "fn_index": fn_index
-        }
-        try:
-            response = await client.post(f"{gradio_url}/api/predict", json=payload, headers=headers)
-            return response.text
-        except httpx.RequestError as e:
-            raise HTTPException(status_code=500, detail=f"Request to Gradio API failed: {e}")
+class TTSForm(BaseModel):
+    voice: str
+    text: str
+    model_preset: str
 
-@app.get("/preset-voice")
-async def predict_from_preset_voices(voice_type: str = "random", model_preset: str = "ultrafast", text: str = "Howdy!", fn_index: int = 0):
-    """
-    Route for predicting text-to-speech from preset voices.
+def chunk_audio(t: torch.Tensor, sample_rate: int, chunk_duration_sec: int) -> List[torch.Tensor]:
+    duration = t.shape[1] / sample_rate
+    num_chunks = 1 + int(duration / chunk_duration_sec)
+    chunks = [t[:, (sample_rate * chunk_duration_sec * i):(sample_rate * chunk_duration_sec * (i + 1))] for i in range(num_chunks)]
+    chunks = [chunk for chunk in chunks if chunk.shape[1] > 0]
+    return chunks
 
-    Args:
-        voice_type (str): The type of preset voice to use.
-        model_preset (str): The model preset for TTS.
-        text (str): The text to be converted to speech.
-        fn_index (int): The fn_index parameter for the Gradio API.
+def tts_main(voice_samples: List[torch.Tensor], text: str, model_preset: str) -> str:
+    gen = tts.tts_with_preset(
+        text,
+        voice_samples=voice_samples,
+        conditioning_latents=None,
+        preset=model_preset
+    )
+    torchaudio.save("generated.wav", gen.squeeze(0).cpu(), TORTOISE_SR_OUT)
+    return "generated.wav"
 
-    Returns:
-        str: Response from the Gradio API.
-    """
-    return await make_gradio_request(voice_type, text, model_preset, fn_index)
+def tts_from_preset(voice: str, text: str, model_preset: str):
+    voice_samples, _ = load_voice(voice)
+    return tts_main(voice_samples, text, model_preset)
 
-@app.get("/custom-voices")
-async def predict_from_custom_voice(path_to_audios: list[str], split_in_chunks: bool = True, text: str = "Howdy!", fn_index: int = 1):
-    """
-    Route for predicting text-to-speech from custom voices.
+def tts_from_files(files: List[UploadFile], do_chunk: bool, text: str, model_preset: str):
+    voice_samples = []
+    temp_files = []
 
-    Args:
-        path_to_audios (list[str]): List of audio file paths or URLs.
-        split_in_chunks (bool): Whether to split audio into chunks.
-        text (str): The text to be converted to speech.
-        fn_index (int): The fn_index parameter for the Gradio API.
+    for file in files:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_files.append(temp_file.name)
+            temp_file.write(file.file.read())
+            audio = load_audio(temp_file.name, TORTOISE_SR_IN)
+            if do_chunk:
+                voice_samples.extend(chunk_audio(audio, TORTOISE_SR_IN, 10))
+            else:
+                voice_samples.append(audio)
 
-    Returns:
-        str: Response from the Gradio API.
-    """
-    return await make_gradio_request(path_to_audios, split_in_chunks, text, fn_index)
+    result = tts_main(voice_samples, text, model_preset)
 
-@app.get("/predict-from-one-file")
-async def predict_from_live_recording(path_to_audio: str, split_in_chunks: bool = True, text: str = "Howdy!", fn_index: int = 2):
-    """
-    Route for predicting text-to-speech from a single audio file.
+    for temp_file in temp_files:
+        os.remove(temp_file)
 
-    Args:
-        path_to_audio (str): Path to the audio file or URL.
-        split_in_chunks (bool): Whether to split audio into chunks.
-        text (str): The text to be converted to speech.
-        fn_index (int): The fn_index parameter for the Gradio API.
+    return result
 
-    Returns:
-        str: Response from the Gradio API.
-    """
-    return await make_gradio_request(path_to_audio, split_in_chunks, text, fn_index)
+def tts_from_recording(recording: List[float], do_chunk: bool, text: str, model_preset: str):
+    sample_rate, audio = recording
+    norm_fix = 1
+
+    if audio.dtype == int:
+        norm_fix = 2**31
+    elif audio.dtype == int:
+        norm_fix = 2**15
+
+    audio = torch.FloatTensor(audio) / norm_fix
+
+    if len(audio.shape) > 1:
+        audio = torch.mean(audio, axis=0).unsqueeze(0)
+
+    audio = torchaudio.transforms.Resample(sample_rate, TORTOISE_SR_IN)(audio)
+
+    if do_chunk:
+        voice_samples = chunk_audio(audio, TORTOISE_SR_IN, 10)
+    else:
+        voice_samples = [audio]
+
+    result = tts_main(voice_samples, text, model_preset)
+
+    return result
+
+@app.post("/tts/preset")
+def tts_preset_endpoint(form: TTSForm):
+    return tts_from_preset(form.voice, form.text, form.model_preset)
+
+@app.post("/tts/files")
+def tts_files_endpoint(
+    files: List[UploadFile] = File(...),
+    do_chunk: bool = Form(...),
+    text: str = Form(...),
+    model_preset: str = Form(...),
+):
+    return tts_from_files(files, do_chunk, text, model_preset)
+
+@app.post("/tts/recording")
+def tts_recording_endpoint(
+    sample_rate: int = Form(...),
+    audio: List[float] = Form(...),
+    do_chunk: bool = Form(...),
+    text: str = Form(...),
+    model_preset: str = Form(...),
+):
+    return tts_from_recording([sample_rate, audio], do_chunk, text, model_preset)
